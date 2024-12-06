251db673e45fa800790ac37b7430094fe12370fa
pedro <pedro_bma@hotmail.com>
2024-12-06 14:11:10 -0300

    assets level 3

--- code/Game.py
+++ code/Game.py
@@ -29,6 +29,9 @@ class Game:
                     level = Level(self.window, 'Level2', menu_return, player_score)
                     level_return = level.run(player_score)
                     if level_return:
+                        level = Level(self.window, 'Level3', menu_return, player_score)
+                        level_return = level.run(player_score)
+                    if level_return:
                         score.save(menu_return, player_score)
 
             elif menu_return == MENU_OPTION[3]:
